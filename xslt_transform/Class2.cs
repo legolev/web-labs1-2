ced06c9225a3ca3727c8015febe5e7eb6f8c527c
o.utekov@gmail.com <o.utekov@gmail.com@21f7a4de-338c-da22-39b0-6069f80d4add>
2014-10-05 01:34:26 +0000

    mn

---
+++ xslt_transform/Class2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+public class XmlTransformUtil
+{
+
+    public static void Main(string[] args)
+    {
+
+        if (args.Length == 2)
+        {
+            Transform(args[0], args[1]);
+        }
+        else
+        {
+            PrintUsage();
+        }
+
+    }
+
+    public static void Transform(string sXmlPath, string sXslPath)
+    {
+
+        try
+        {
+
+            //подгружаем xml
+            XPathDocument myXPathDoc = new XPathDocument(sXmlPath);
+
+            XslTransform myXslTrans = new XslTransform();
+
+            //подгружаем xslt
+            myXslTrans.Load(sXslPath);
+
+            //на выход в html
+            XmlTextWriter myWriter = new XmlTextWriter
+                ("result.html", null);
+
+            //преобразование
+            myXslTrans.Transform(myXPathDoc, null, myWriter);
+
+            myWriter.Close();
+
+
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine("Exception: {0}", e.ToString());
+        }
+
+    }
+
+
+    public static void PrintUsage()
+    {
+        Console.WriteLine
+        ("Usage: XmlTransformUtil.exe <xml path> <xsl path>");
+    }
+
+}
